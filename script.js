e0d4bf9f84ad0333f1a0a2eeeecbde36ed92a1a0
Garett Haight <garett.haight@gmail.com>
2017-09-16 23:40:23 -0700

    channel shifting is now directional on the X axis

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-var img = new Image();
-img.src = 'rhino.jpg';
-var canvas = document.getElementById('c');
-var ctx = canvas.getContext('2d');
-var data;
-var imageData;
-img.onload = function() {
-  ctx.drawImage(img, 0, 0);
-  img.style.display = 'none';
-  imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  data = imageData.data;
-};
 
-
-console.log(data);
 
 var invert = function() {
     for (var i = 0; i < data.length; i += 4) {
@@ -33,51 +19,68 @@ var invert = function() {
     ctx.putImageData(imageData, 0, 0);
   };
 
-  var shiftChannel = function(shift, channel) {
-    if(typeof shift === 'undefined') {
-      shift = 1;
+  
+
+  class ImageEffect {
+    constructor(img, canvas){
+
+      this.canvas = document.getElementById(canvas);
+      this.ctx = this.canvas.getContext('2d');
+      this.data;
+      this.imageData;
+      this.img = new Image();
+      this.img.src = 'rhino.jpg';
+      this.img.onload = () => {
+        this.ctx.drawImage(this.img, 0, 0);
+        this.img.style.display = 'none';
+        this.imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+        this.data = this.imageData.data;
+      };
     }
-    // default is value for red
-    var start = 0;;
-    switch(channel){
-      case 'green':
-        start = 1;
-        break;
-      case 'blue':
-        start = 2;
-        break;
-      case alpha:
-        start = 3;
-        break;
-    }
-    // rows
-    for(var i = 0; i < img.height; i++) {
-      console.log(i);
-      //columns
-      for(var j = (i * img.width * 4) + start; j < (i+1) * img.width * 4; j+=4) {
-        if(data[j + 4 * shift] !== null) {
-          data[j] = data[j + 4];
+
+    shiftChannel(shift, channel) {
+      if(typeof shift === 'undefined') {
+        shift = 1;
+      }
+
+      // default is value for red
+      var start = 0;
+      var newImg = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+      var newData = newImg.data
+      switch(channel){
+        case 'green':
+          start = 1;
+          break;
+        case 'blue':
+          start = 2;
+          break;
+        case 'alpha':
+          start = 3;
+          break;
+      }
+      
+      // rows
+      for(var i = 0; i < this.img.height; i++) {
+        //columns
+        for(var j = (i * this.img.width * 4) + start; j < (i+1) * this.img.width * 4; j+=4) {
+          // shift right
+          // j is the first channel of the current pixel
+          newData[j] = this.data[j + (4 * -shift)];
         }
       }
+      this.imageData = newImg;
+      this.data = newData;
+      this.ctx.putImageData(newImg, 0, 0);
     }
-
-    // red channel vertical gradient
-    // for(var i = 0; i < data.length; i+=4) {
-    //   var r = i / data.length;
-    //   data[i] = Math.round(255 * r);
-    // }
-
-
-
-
-    // vertical stripes
-    // for(var i = 0; i < data.length; i += 4) {;
-    //   if((i/4) % 2) {
-    //     data[i] = 255;
-    //   }
-    //   else {
-    //     data[i] = 0;
-    //   }
-    // }
-    ctx.putImageData(imageData, 0, 0);
   }
+
+  var ie = new ImageEffect('rhino.jpg', 'c');
+
+  document.addEventListener('keydown', (e) => {
+    if(e.keyCode === 39) {
+      ie.shiftChannel(1, 'red');
+    }
+    else if(e.keyCode === 37) {
+      ie.shiftChannel(-1, 'red');
+    }
+  });
